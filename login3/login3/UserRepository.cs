fdf24a1aa44529da05655a636cfe8436db2fc541
MNFI13-blode <118786703+MNFI13-blode@users.noreply.github.com>
2023-05-14 18:25:35 +0700

    Login

---
+++ login3/login3/UserRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyApp.Model;
+
+
+namespace MyApp.Repository
+{
+    public class UserRepository
+    {
+        // Deklarasi variabel dengan nilai default
+        private User[] users = new User[]
+        {
+            new User { Id = 1, Username = "admin", Password = "admin" },
+            new User { Id = 2, Username = "user", Password = "user" }
+        };
+
+        // Method untuk memvalidasi user dan password
+        public bool ValidateUser(User user)
+        {
+            foreach (User u in users)
+            {
+                if (u.Username == user.Username && u.Password == user.Password)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
